14f143cff1692c035b48cf45c2f4b05790f8ed7e
Kenneth Powers <ken@kenpowers.net>
2015-05-13 22:01:47 -0400

    Add directory aliases.

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -1,3 +1,28 @@
 # Custom aliases
 ls --color -d . &>/dev/null 2>&1 && alias ls='ls --color=tty' || alias ls='ls -G'
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
 
+alias 1='cd -'
+alias 2='cd -2'
+alias 3='cd -3'
+alias 4='cd -4'
+alias 5='cd -5'
+alias 6='cd -6'
+alias 7='cd -7'
+alias 8='cd -8'
+alias 9='cd -9'
+
+alias md='mkdir -p'
+alias rd=rmdir
+alias d='dirs -v | head -10'
+
+alias lsa='ls -lah'
+alias l='ls -lah'
+alias ll='ls -lh'
+alias la='ls -lAh'
+
+alias pu='pushd'
+alias po='popd'
