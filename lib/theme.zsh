de912479fdc7e21214152b224d2c0394d1cd04ed
Kenneth Powers <ken@kenpowers.net>
2015-05-20 13:56:17 -0400

    Make prompt theme use standard terminal colors.

--- lib/theme.zsh
+++ lib/theme.zsh
@@ -1,21 +1,5 @@
-# Based on steeef
+# Originally based on steeef
 # https://github.com/sorin-ionescu/prezto/blob/master/modules/prompt/functions/prompt_steeef_setup
-
-# Colors
-## Use extended color pallete if available.
-if [[ $TERM = *256color* || $TERM = *rxvt* ]]; then
-  turquoise="%F{81}"
-  orange="%F{166}"
-  purple="%F{135}"
-  hotpink="%F{161}"
-  limegreen="%F{118}"
-else
-  turquoise="$fg[cyan]"
-  orange="$fg[yellow]"
-  purple="$fg[magenta]"
-  hotpink="$fg[red]"
-  limegreen="$fg[green]"
-fi
 
 # Functions
 ## Working Directory
@@ -26,7 +10,7 @@ function get_pwd() {
 function git_dirty() {
   if [[ -n $(git status -s 2> /dev/null) ]]; then
     # Dirty
-    echo "$hotpink●"
+    echo "$fg[red]●"
   fi
 }
 function git_branch() {
@@ -36,11 +20,11 @@ function git_branch() {
 }
 function git_prompt() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
-  echo " ($turquoise$(git_branch)$(git_dirty)$reset_color)"
+  echo " ($fg[cyan]$(git_branch)$(git_dirty)$reset_color)"
 }
 
 # Sets the prompt
 function precmd() {
-  PROMPT="$purple%n$reset_color at $orange%m$reset_color in $limegreen$(get_pwd)$reset_color$(git_prompt)
+  PROMPT="$fg[magenta]%n$reset_color at $fg[yellow]%m$reset_color in $fg[green]$(get_pwd)$reset_color$(git_prompt)
 %{$reset_color%}$ "
 }
