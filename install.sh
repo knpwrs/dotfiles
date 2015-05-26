b184dc87f7a2a804ed4db0c4a09699b4223bc471
Kenneth Powers <ken@kenpowers.net>
2015-05-26 08:48:38 -0400

    Update install script to remove dead links.

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env zsh
 
+# Use extended globbing
 setopt EXTENDED_GLOB
+
+# Remove all dead links in home directory.
+rm "${ZDOTDIR:-$HOME}"/*(-@D)
+
+# Link all dotfiles to those in this repo.
 for rcfile in "${ZDOTDIR:-$HOME}"/.dotfiles/home/.*; do
   ln -fsv "$rcfile" "${ZDOTDIR:-$HOME}/${rcfile:t}"
 done
