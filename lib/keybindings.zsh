0bc24d373f4fd629f24789ae76975baaa29f568a
Kenneth Powers <ken@kenpowers.net>
2015-06-06 12:59:44 -0400

    Add history keybindings.

--- lib/keybindings.zsh
+++ lib/keybindings.zsh
@@ -1,6 +1,10 @@
 # Emacs key bindings
 bindkey -e
 
+# History key bindings
+bindkey '^[[A' up-line-or-search
+bindkey '^[[B' down-line-or-search
+
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
