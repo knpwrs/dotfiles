61a55d9d99ab21d991d7a20eb4206b2faa1e4f99
Ken Powers <ken@kenpowers.net>
2015-10-13 20:32:13 -0400

    Add Homebrew Cask applications.

--- vendor/packages/install-homebrew.sh
+++ vendor/packages/install-homebrew.sh
@@ -3,3 +3,4 @@
 hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" }
 brew install ack caskroom/cask/brew-cask cowsay ctags git git-lfs ranger ssh-copy-id tmux tree vim zsh
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook suspicious-package provisionql
+brew cask install atom alfred beyond-compare bit-slicer firefox google-chrome handbrake keka reaper vlc
