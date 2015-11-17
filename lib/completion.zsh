8d5a9750182060df23f4160506e31e0933d4eae3
Kenneth Powers <ken@kenpowers.net>
2015-11-17 16:59:45 -0500

    Make shift-tab work in autocomplete menu.

--- lib/completion.zsh
+++ lib/completion.zsh
@@ -11,3 +11,6 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:
 zstyle ':completion:*:*:*:*:*' menu select
 zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;34=0=01'
 zstyle ':completion:*:*:*:*:processes' command "ps -u $USER -o pid,user,comm -w -w"
+## Shift-tab to go in reverse
+zmodload zsh/complist
+bindkey -M menuselect '^[[Z' reverse-menu-complete
