8940d5e935e83775ca11967559493a678f14cba0
Ken Powers <ken@kenpowers.net>
2016-01-08 20:17:11 -0500

    Add comments to homebrew packages.

--- vendor/packages/homebrew/install.sh
+++ vendor/packages/homebrew/install.sh
@@ -3,10 +3,10 @@
 if  [[ "$OSTYPE" = darwin* ]]; then
   HBPACKAGES=$DOTPACKAGES/homebrew
   hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" }
-  <$HBPACKAGES/packages-homebrew xargs brew install
-  <$HBPACKAGES/packages-dupes xargs brew install
-  <$HBPACKAGES/packages-cask xargs brew cask install
-  <$HBPACKAGES/packages-cask-ql xargs brew cask install
+  <$HBPACKAGES/packages-homebrew cut -d' ' -f1 | xargs brew install
+  <$HBPACKAGES/packages-dupes cut -d' ' -f1 | xargs brew install
+  <$HBPACKAGES/packages-cask cut -d' ' -f1 | xargs brew cask install
+  <$HBPACKAGES/packages-cask-ql cut -d' ' -f1 | xargs brew cask install
 else
   echo "Not on OSX. Skipping Homebrew."
 fi
