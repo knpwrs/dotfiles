7f21492ab886ef277fd714e2bec8c575a3d40cea
Kenneth Powers <ken@kenpowers.net>
2016-01-08 20:19:03 -0500

    Add comments to ruby gems.

--- vendor/packages/gems/install.sh
+++ vendor/packages/gems/install.sh
@@ -4,4 +4,4 @@ echo "---"
 echo "!!! sudo required to install gems using system ruby."
 echo "---"
 
-<$DOTPACKAGES/gems/packages xargs sudo gem install
+<$DOTPACKAGES/gems/packages cut -d' ' -f1 | xargs sudo gem install
