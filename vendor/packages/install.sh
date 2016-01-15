afdc7daac086cd133e830827c66a09e8804f8af1
Kenneth Powers <ken@kenpowers.net>
2016-01-14 20:46:43 -0500

    Forever sudo on package install script.

--- vendor/packages/install.sh
+++ vendor/packages/install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env zsh
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 $DOTPACKAGES/homebrew/install.sh
 $DOTPACKAGES/npm/install.sh
 $DOTPACKAGES/gems/install.sh
