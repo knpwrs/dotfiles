840281d0ac8324fa4d64e8f0ec7d6501e625e1e0
Ken Powers <ken@kenpowers.net>
2016-03-29 19:49:11 -0400

    Clean up homebrew install script.

--- vendor/packages/homebrew/install.sh
+++ vendor/packages/homebrew/install.sh
@@ -1,15 +1,23 @@
 #!/usr/bin/env zsh
 
+function installhb() {
+  <$DOTPACKAGES/homebrew/packages-$1 cut -d' ' -f1 | xargs brew $2 install
+}
+
+function installhbc() {
+  installhb $1 cask
+}
+
 if  [[ "$OSTYPE" = darwin* ]]; then
   HBPACKAGES=$DOTPACKAGES/homebrew
   hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && echo "Please agree to the Xcode license." && sudo xcodebuild -license }
-  <$HBPACKAGES/packages-homebrew cut -d' ' -f1 | xargs brew install
-  <$HBPACKAGES/packages-dupes cut -d' ' -f1 | xargs brew install
-  <$HBPACKAGES/packages-games cut -d' ' -f1 | xargs brew install
-  <$HBPACKAGES/packages-cask cut -d' ' -f1 | xargs brew cask install
-  <$HBPACKAGES/packages-cask-ql cut -d' ' -f1 | xargs brew cask install
+  installhb homebrew
+  installhb dupes
+  installhb games
+  installhbc cask
+  installhbc cask-ql
   brew tap caskroom/fonts
-  <$HBPACKAGES/packages-cask-fonts cut -d' ' -f1 | xargs brew cask install
+  installhbc cask-fonts
 else
   echo "Not on OSX. Skipping Homebrew."
 fi
