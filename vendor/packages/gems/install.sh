712e9a5e068fefebd123cac40b110a1eb7086cac
Ken Powers <ken@kenpowers.net>
2016-08-29 12:16:39 -0400

    Clean up gem installation.

--- vendor/packages/gems/install.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env zsh
-
-echo "---"
-echo "!!! sudo required to install gems using system ruby."
-echo "---"
-
-<$DOTPACKAGES/gems/packages cut -d' ' -f1 | xargs sudo gem install
---
+++ vendor/packages/install-gems.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+echo "---"
+echo "!!! sudo required to install gems using system ruby."
+echo "---"
+
+gems=(
+  bropages           # Highly readable supplement to man pages.
+  cocoapods          # Package manager for XCode projects.
+  jekyll             # A static site generator.
+  jekyll-paginate    # Pagination plugin for jekyll.
+  octopress-debugger # A debugger for jeykll.
+)
+
+sudo gem install "${gems[@]///}"
