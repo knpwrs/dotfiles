0044cf11efd74a2c291cf8528bfca4c8bcfa0bd0
Ken Powers <ken@kenpowers.net>
2016-09-10 18:16:23 -0400

    Wrap nvm lib in if statement.

--- lib/nvm.zsh
+++ lib/nvm.zsh
@@ -3,17 +3,18 @@
 # Set up NVM
 export NVM_DIR="$HOME/.nvm"
 export NVM_SH="$NVM_DIR/nvm.sh"
-# https://github.com/creationix/nvm/issues/860
-declare -a NODE_GLOBALS=(`find $NVM_DIR/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+if [ -f "$NVM_SH" ]; then
+  # https://github.com/creationix/nvm/issues/860
+  declare -a NODE_GLOBALS=(`find $NVM_DIR/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
 
-NODE_GLOBALS+=("node")
-NODE_GLOBALS+=("nvm")
+  NODE_GLOBALS+=("node")
+  NODE_GLOBALS+=("nvm")
 
-load_nvm () {
-  [ -s "$NVM_SH" ] && . "$NVM_SH"
-}
+  load_nvm () {
+    [ -s "$NVM_SH" ] && . "$NVM_SH"
+  }
 
-for cmd in "${NODE_GLOBALS[@]}"; do
-  eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
-done
-
+  for cmd in "${NODE_GLOBALS[@]}"; do
+    eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
+  done
+fi
