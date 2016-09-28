4ae20a0a6eee900f39f141d6054d8920586a7209
Ken Powers <ken@kenpowers.net>
2016-09-27 20:14:42 -0400

    Add lolcat gem.

--- vendor/packages/install-gems.sh
+++ vendor/packages/install-gems.sh
@@ -9,6 +9,7 @@ gems=(
   cocoapods          # Package manager for XCode projects.
   jekyll             # A static site generator.
   jekyll-paginate    # Pagination plugin for jekyll.
+  lolcat             # (lol)cat
   octopress-debugger # A debugger for jeykll.
 )
 
