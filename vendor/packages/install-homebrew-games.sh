038b1399c8f1531e7114aa6ddd92324a8961c132
Kenneth Powers <ken@kenpowers.net>
2016-12-21 23:56:24 -0500

    Switch to homebrew bundle.

--- vendor/packages/install-homebrew-games.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-formulae=(
-  homebrew/games/gnuski # Open source clone of Skifree
-  homebrew/games/ski    # Evade the deadly Yeti on your jet-powered skis.
-  homebrew/games/yetris # Terminal Tetris
-)
-
-brew install "${formulae[@]///}"
--- vendor/packages/install-mac.sh
+++ vendor/packages/install-mac.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env zsh
 
 if  [[ "$OSTYPE" = darwin* ]]; then
-  HBPACKAGES=$DOTPACKAGES/homebrew
   hash brew 2>/dev/null || { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" && echo "Please agree to the Xcode license." && sudo xcodebuild -license }
-  $DOTPACKAGES/install-homebrew-main.sh
-  $DOTPACKAGES/install-homebrew-dupes.sh
-  $DOTPACKAGES/install-homebrew-cask.sh
-  $DOTPACKAGES/install-homebrew-cask-ql.sh
-  $DOTPACKAGES/install-homebrew-cask-fonts.sh
-  $DOTPACKAGES/install-homebrew-games.sh
-  $DOTPACKAGES/install-homebrew-external.sh
-  $DOTPACKAGES/install-mas.sh
+  brew update
+  brew upgrade
+  brew bundle
+  brew cleanup
 else
   echo "Not on macOS. Skipping mac packages."
 fi
