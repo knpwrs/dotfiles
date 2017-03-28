f8a77f58164019913b4f0abb8d6e96048731b357
Ken Powers <ken@kenpowers.net>
2017-03-28 15:14:53 -0400

    Remove node debugging tools since node can now be remote debugged with Chrome.

--- vendor/packages/install-npm.sh
+++ vendor/packages/install-npm.sh
@@ -14,7 +14,6 @@ fi
 
 packages=(
   browser-run     # Runs JavaScript in a browser and sends the console logs to stdout.
-  bugger          # Chrome devtool bindings for node.
   david           # NPM dependency version checker.
   dev-time-cli    # Get the current local time of a GitHub user.
   empty-trash-cli # Empty trash.
@@ -32,7 +31,6 @@ packages=(
   np              # Better npm publish.
   npm-name-cli    # Check if a package name is available on npm.
   npm-quick-run   # Quickly run npm scripts by prefix without typing the full name.
-  node-inspector  # Node debugger.
   nws             # Node web server.
   pageres-cli     # Capture screenshots of websites at various resolutions.
   pen.md          # Hot-loading markdown preview server.
