8c14abc0f4a27680ed6019e177b9874632b7fc80
Ken Powers <ken@kenpowers.net>
2018-04-06 14:50:48 -0400

    Add gbd alias.

--- lib/git.zsh
+++ lib/git.zsh
@@ -55,6 +55,7 @@ alias gap='git add -p'
 
 alias gb='git branch'
 alias gba='git branch -a'
+alias gbd='git branch -D'
 function gbda() {
   local branches=$(git branch --no-color --merged | grep -v "\*" | grep -v master | grep -v svn)
   if [ "$1" = "-r" ]; then
