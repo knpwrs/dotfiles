b38aab5452386f4da18100410224ef24b97aed31
Ken Powers <ken@kenpowers.net>
2018-05-30 09:01:16 -0400

    Add hyperfine crate.

--- vendor/packages/install-crates.sh
+++ vendor/packages/install-crates.sh
@@ -5,7 +5,8 @@ CARGO="$HOME/.cargo/bin/cargo"
 RUSTUP="$HOME/.cargo/bin/rustup"
 
 crates=(
-  racer # Code completion for Rust
+  racer     # Code completion for rust
+  hyperfine # A command-line benchmarking tool
 )
 
 $CARGO add component rust-src
