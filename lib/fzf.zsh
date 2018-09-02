1c7e75141e5452251e4c8e752d6d1fa8cfbe5331
Kenneth Powers <ken@kenpowers.net>
2018-09-02 19:38:08 -0400

    Replace fim with fe/fo.

--- lib/fzf.zsh
+++ lib/fzf.zsh
@@ -104,8 +104,27 @@ chh() {
   fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs $open > /dev/null 2> /dev/null
  }
 
- # Search words
- alias fw='fzf < /usr/share/dict/words'
+# Search words
+alias fw='fzf < /usr/share/dict/words'
 
- # Open a file in vim
- alias fim='vim $(fzf)'
+# fe [FUZZY PATTERN] - Open the selected file with the default editor
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fe() {
+  local files
+  IFS=$'\n' files=($(fzf --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && ${EDITOR:-vim} "${files[@]}"
+}
+
+# Modified version where you can press
+#   - CTRL-O to open with `open` command,
+#   - CTRL-E or Enter key to open with the $EDITOR
+fo() {
+  local out file key
+  IFS=$'\n' out=($(fzf --query="$1" --exit-0 --expect=ctrl-o,ctrl-e))
+  key=$(head -1 <<< "$out")
+  file=$(head -2 <<< "$out" | tail -1)
+  if [ -n "$file" ]; then
+    [ "$key" = ctrl-o ] && open "$file" || ${EDITOR:-vim} "$file"
+  fi
+}
