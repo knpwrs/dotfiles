95e03edccc668af0dbc2d335a00c4cea7457e379
Kenneth Powers <ken@kenpowers.net>
2018-09-02 19:40:41 -0400

    Add fa alias (fuzzy git add).

--- lib/git.zsh
+++ lib/git.zsh
@@ -396,3 +396,12 @@ fsha() {
   commit=$(echo "$commits" | fzf --tac +s +m -e --ansi --reverse) &&
   echo -n $(echo "$commit" | sed "s/ .*//")
 }
+
+# fa [FUZZY PATTERN] - Add file to git staging area
+#   - Bypass fuzzy finder if there's only one match (--select-1)
+#   - Exit if there's no match (--exit-0)
+fa() {
+  local files
+  IFS=$'\n' files=($(fzf --query="$1" --multi --select-1 --exit-0))
+  [[ -n "$files" ]] && git add "${files[@]}"
+}
