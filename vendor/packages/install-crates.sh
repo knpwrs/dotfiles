249b3a52379a425c1a839448a87060fa98d647e9
Kenneth Powers <ken@kenpowers.net>
2018-09-03 20:09:13 -0400

    Add cargo-edit crate.

--- vendor/packages/install-crates.sh
+++ vendor/packages/install-crates.sh
@@ -5,6 +5,7 @@ CARGO="$HOME/.cargo/bin/cargo"
 RUSTUP="$HOME/.cargo/bin/rustup"
 
 crates=(
+  cargo-edit  # Add, remove, and upgrade cargo dependencies from the command line
   cargo-watch # Watch for file changes and run cargo commands
   hyperfine   # A command-line benchmarking tool
   racer       # Code completion for rust
