12a4513c80a30f02f8a88f3dba84571c0b0f9996
Ken Powers <ken@kenpowers.net>
2018-10-17 10:58:29 -0400

    Add setup notes to jira.zsh

--- lib/jira.zsh
+++ lib/jira.zsh
@@ -1,5 +1,23 @@
 #!/usr/bin/env zsh
 
+#
+# You will need the `jq` and `fzf` commands installed in order for these
+# functions to work:
+#
+#   - https://stedolan.github.io/jq/
+#   - https://github.com/junegunn/fzf
+#
+# You will also need to define $JIRA_TEAM and $JIRA_TOKEN environment
+# variables. See the following for information on setting machine-private
+# environment variables:
+#
+#   - https://github.com/knpwrs/dotfiles#private-environment-variables
+#
+# See the following for information on creating a JIRA access token:
+#
+#  - https://confluence.atlassian.com/cloud/api-tokens-938839638.html ( https://archive.is/vjoEZ )
+#
+
 function jql() {
   curl "https://$JIRA_TEAM.atlassian.net/rest/api/2/search?jql=$1&maxResults=100&startAt=$(($2 * 100))" \
     -s -u "$JIRA_USER:$JIRA_TOKEN" \
