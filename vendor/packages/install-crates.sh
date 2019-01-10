95cc2b7ba2c28bac92af5787f787e06cbc4e2543
Ken Powers <ken@kenpowers.net>
2019-01-10 08:23:13 -0500

    Add hexyl crate.

--- vendor/packages/install-crates.sh
+++ vendor/packages/install-crates.sh
@@ -8,6 +8,7 @@ crates=(
   bat         # A cat clone with wings
   cargo-edit  # Add, remove, and upgrade cargo dependencies from the command line
   cargo-watch # Watch for file changes and run cargo commands
+  hexyl       # A command-line hex viewer
   hyperfine   # A command-line benchmarking tool
   racer       # Code completion for rust
 )
