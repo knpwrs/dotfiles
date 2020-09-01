978e0980ae4d390ffeb305853d9b8e09c35376be
Ken Powers <ken@kenpowers.net>
2020-09-01 11:24:45 -0400

    Show hidden files in default fzf command

--- lib/fzf.zsh
+++ lib/fzf.zsh
@@ -1,7 +1,7 @@
 #!/usr/local/bin zsh
 
 # Use fd instead of find for speed
-export FZF_DEFAULT_COMMAND='fd --type f'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Most of these are from https://github.com/junegunn/fzf/wiki/examples
