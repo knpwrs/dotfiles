8cc6cc28661150ce8e0938149e58031e5869c502
Kenneth Powers <ken@kenpowers.net>
2021-04-29 15:39:55 -0500

    navi(misc): add format JSON on clipboard snippet

--- lib/aliases.zsh
+++ lib/aliases.zsh
@@ -170,7 +170,6 @@ alias ip6='dig @ns1.google.com TXT o-o.myaddr.l.google.com +short -6'
 
 # Pasteboard jq
 alias pbjq='pbpaste | jq'
-alias pbjqpb='pbjq | pbcopy'
 
 # Make aliases available to sudo
 alias sudo='sudo '
