020aaa94c7a350da6a44bf8e4fc2d944555320fd
Ken Powers <ken@kenpowers.net>
2021-08-25 15:26:13 -0500

    nvim: use telescope for certain lsp keymaps

--- home/config/nvim/lua/p-nvim-lspconfig.lua
+++ home/config/nvim/lua/p-nvim-lspconfig.lua
@@ -4,19 +4,24 @@ local nvim_set_keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- See `:help vim.lsp.*` for documentation on any of the below functions
-nvim_set_keymap('n', 'gD', '<Cmd>lua vim.lsp.buf.declaration()<Cr>', opts)
-nvim_set_keymap('n', 'gd', '<Cmd>lua vim.lsp.buf.definition()<Cr>', opts)
 nvim_set_keymap('n', 'K', '<Cmd>lua vim.lsp.buf.hover()<Cr>', opts)
 nvim_set_keymap('n', 'gi', '<Cmd>lua vim.lsp.buf.implementation()<Cr>', opts)
 nvim_set_keymap('n', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<Cr>', opts)
 nvim_set_keymap('n', 'gy', '<Cmd>lua vim.lsp.buf.type_definition()<Cr>', opts)
 nvim_set_keymap('n', '<Leader>rn', '<Cmd>lua vim.lsp.buf.rename()<Cr>', opts)
 nvim_set_keymap('n', '<Leader>ca', '<Cmd>lua vim.lsp.buf.code_action()<Cr>', opts)
-nvim_set_keymap('n', 'gr', '<Cmd>lua vim.lsp.buf.references()<Cr>', opts)
 nvim_set_keymap('n', '[d', '<Cmd>lua vim.lsp.diagnostic.goto_prev()<Cr>', opts)
 nvim_set_keymap('n', ']d', '<Cmd>lua vim.lsp.diagnostic.goto_next()<Cr>', opts)
 nvim_set_keymap('n', '<Leader>q', '<Cmd>lua vim.lsp.diagnostic.set_loclist()<Cr>', opts)
 nvim_set_keymap('n', '<Leader>f', '<Cmd>lua vim.lsp.buf.formatting()<Cr>', opts)
+
+-- Telescope lsp maps
+nvim_set_keymap('n', 'gd', '<Cmd>Telescope lsp_definitions<Cr>', opts)
+nvim_set_keymap('n', 'gr', '<Cmd>Telescope lsp_references<Cr>', opts)
+nvim_set_keymap('n', 'gp', '<Cmd>Telescope lsp_document_symbols<Cr>', opts)
+nvim_set_keymap('n', 'ge', '<Cmd>Telescope lsp_code_actions<Cr>', opts)
+nvim_set_keymap('n', 'gD', '<Cmd>Telescope lsp_document_diagnostics<Cr>', opts)
+nvim_set_keymap('n', 'gI', '<Cmd>Telescope lsp_implementations<Cr>', opts)
 
 local function setup_servers()
   require('lspinstall').setup()
