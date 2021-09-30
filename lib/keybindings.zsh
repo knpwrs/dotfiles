3a7bed07c3c465a89588f320912480c0a334c652
Ken Powers <ken@kenpowers.net>
2021-09-30 10:14:22 -0500

    zsh: remove ^j and ^k in favor of default ^n and ^p

--- lib/keybindings.zsh
+++ lib/keybindings.zsh
@@ -1,10 +1,6 @@
 # Emacs key bindings
 bindkey -e
 
-# History key bindings
-bindkey '^k' up-line-or-search
-bindkey '^j' down-line-or-search
-
 # Edit the current command line in $EDITOR
 autoload -U edit-command-line
 zle -N edit-command-line
