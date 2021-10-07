9d1782ccf780579f2e65e1cdb49fb648763884a9
Ken Powers <ken@kenpowers.net>
2021-10-07 11:30:02 -0500

    refactor(nvim(lightspeed)): upgrade to built-in (experimental) `;` and `,` <Plug> mappings

--- home/config/nvim/lua/plugins/lightspeed.lua
+++ home/config/nvim/lua/plugins/lightspeed.lua
@@ -6,14 +6,8 @@ nvim_set_keymap('n', 'Z', '<Plug>Lightspeed_S', {})
 nvim_set_keymap('x', 'z', '<Plug>Lightspeed_s', {})
 nvim_set_keymap('x', 'Z', '<Plug>Lightspeed_S', {})
 
-function repeat_ft(reverse)
-  local ls = require'lightspeed'
-  ls.ft['instant-repeat?'] = true
-  ls.ft:to(reverse, ls.ft['prev-t-like?'])
-end
-
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', ';', '<cmd>lua repeat_ft(false)<cr>', opts)
-vim.api.nvim_set_keymap('x', ';', '<cmd>lua repeat_ft(false)<cr>', opts)
-vim.api.nvim_set_keymap('n', ',', '<cmd>lua repeat_ft(true)<cr>', opts)
-vim.api.nvim_set_keymap('x', ',', '<cmd>lua repeat_ft(true)<cr>', opts)
+local opts = { silent = true }
+vim.api.nvim_set_keymap('n', ';', '<Plug>Lightspeed_;_ft', opts)
+vim.api.nvim_set_keymap('x', ';', '<Plug>Lightspeed_;_ft', opts)
+vim.api.nvim_set_keymap('n', ',', '<Plug>Lightspeed_,_ft', opts)
+vim.api.nvim_set_keymap('x', ',', '<Plug>Lightspeed_,_ft', opts)
