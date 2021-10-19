88659cc87e5dbc7643e39e3b7eb5a2609a0b1ea4
Ken Powers <ken@kenpowers.net>
2021-10-19 15:10:51 -0500

    nvim(lightspeed): refactor config

--- home/config/nvim/lua/plugins/lightspeed.lua
+++ home/config/nvim/lua/plugins/lightspeed.lua
@@ -1,12 +1,13 @@
 local cmd = vim.cmd
 local nvim_set_keymap = vim.api.nvim_set_keymap
 
-nvim_set_keymap('n', 'z', '<Plug>Lightspeed_s', {})
-nvim_set_keymap('n', 'Z', '<Plug>Lightspeed_S', {})
-nvim_set_keymap('x', 'z', '<Plug>Lightspeed_s', {})
-nvim_set_keymap('x', 'Z', '<Plug>Lightspeed_S', {})
-
 local opts = { silent = true }
+
+nvim_set_keymap('n', 'z', '<Plug>Lightspeed_s', opts)
+nvim_set_keymap('n', 'Z', '<Plug>Lightspeed_S', opts)
+nvim_set_keymap('x', 'z', '<Plug>Lightspeed_x', opts)
+nvim_set_keymap('x', 'Z', '<Plug>Lightspeed_X', opts)
+
 vim.api.nvim_set_keymap('n', ';', '<Plug>Lightspeed_;_ft', opts)
 vim.api.nvim_set_keymap('x', ';', '<Plug>Lightspeed_;_ft', opts)
 vim.api.nvim_set_keymap('n', ',', '<Plug>Lightspeed_,_ft', opts)
