1f53eddfa76a21ceea23483c01fe4c036e96f4ee
Ken Powers <ken@kenpowers.net>
2021-11-04 10:29:18 -0500

    nvim(grepper): move GrepperOperator outside of <Leader> prefix

--- home/config/nvim/lua/plugins/vim-grepper.lua
+++ home/config/nvim/lua/plugins/vim-grepper.lua
@@ -12,11 +12,20 @@ wk.register(
   {
     g = { '<Cmd>Grepper<Cr>', 'Grepper' },
     G = { '<Cmd>Grepper -buffers<Cr>', 'Grepper Buffers' },
-    gs = { '<Plug>(GrepperOperator)', 'Grepper Operator' },
     ['*'] = { '<Cmd>Grepper -cword -noprompt<Cr>', 'Grepper Word Under Cursor' },
   },
   {
     prefix = '<Leader>',
+  }
+)
+
+
+wk.register(
+  {
+    gs = { '<Plug>(GrepperOperator)', 'Grepper Operator' },
+  },
+  {
+    mode = 'n',
   }
 )
 
