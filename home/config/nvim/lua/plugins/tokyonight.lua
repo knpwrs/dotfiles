f60e61c80191c13832d7c9b4d1d1d8f0ecafd556
Ken Powers <ken@kenpowers.net>
2021-12-14 15:06:25 -0600

    nvim(theme): highlight yanks

--- home/config/nvim/lua/plugins/tokyonight.lua
+++ home/config/nvim/lua/plugins/tokyonight.lua
@@ -1,4 +1,15 @@
-vim.g.tokyonight_style = 'night'
-vim.g.tokyonight_colors = { bg = '#121212', bg_dark = '#101010', bg_sidebar = '#101010' }
+local g = vim.g
+local cmd = vim.cmd
 
-vim.cmd[[colorscheme tokyonight]]
+g.tokyonight_style = 'night'
+g.tokyonight_colors = { bg = '#121212', bg_dark = '#101010', bg_sidebar = '#101010' }
+
+cmd[[colorscheme tokyonight]]
+
+-- Highlight yanked regions
+cmd([[
+  augroup HighlightYank
+    autocmd!
+    au TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
+  augroup END
+]])
