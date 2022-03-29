63d8b71bf9aaa9b3188f5e889d4302c06fcb8327
Ken Powers <ken@kenpowers.net>
2022-03-29 13:55:18 -0500

    feat(gh): add gh-prs extension

--- vendor/packages/install-gh.sh
+++ vendor/packages/install-gh.sh
@@ -4,6 +4,7 @@ hash gh 2>/dev/null || { echo "Please install gh before installing gh extensions
 
 extensions=(
   davidraviv/gh-clean-branches # Safely delete local branches that have no remotes and no hanging changes.
+  dlvhdr/gh-prs                # CLI PR Dashboard
 )
 
 echo "${extensions[@]///}" | xargs -n1 gh extension install
