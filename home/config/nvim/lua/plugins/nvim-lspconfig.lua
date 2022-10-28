9c6c0d0dd7b280da173e4e53144c19517493ece8
Ken Powers <ken@kenpowers.net>
2022-10-28 10:10:17 -0500

    nvim(cmp): s|update|default|

--- home/config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -46,7 +46,7 @@ wk.register(
 )
 
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 lspinstaller.setup()
 
