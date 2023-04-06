f7984c87c4a198d8fb281e92f59eed45d0484d6a
Ken Powers <ken@kenpowers.net>
2023-04-06 11:02:50 -0500

    WIP: fix cmp and copilot

--- home/config/nvim/lua/plugins/nvim-cmp.lua
+++ home/config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,6 +2,8 @@ local nvim_set_keymap = vim.api.nvim_set_keymap
 local cmp = require('cmp')
 local lspkind = require('lspkind')
 
+vim.g.copilot_no_tab_map = true
+
 cmp.setup {
   snippet = {
     expand = function(args)
@@ -12,16 +14,20 @@ cmp.setup {
     ['<C-y>'] = cmp.mapping.confirm({ select = true }),
     ['<C-j>'] = cmp.mapping.scroll_docs(-4),
     ['<C-k>'] = cmp.mapping.scroll_docs(4),
-    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<Cr>'] = cmp.mapping.confirm({ select = true }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      vim.api.nvim_feedkeys(vim.fn['copilot#Accept'](vim.api.nvim_replace_termcodes('<Tab>', true, true, true)), 'n', true)
+    end),
   }),
   formatting = {
-    format = function(entry, vim_item)
-      vim_item.kind = lspkind.presets.default[vim_item.kind]
-      return vim_item
-    end
+    format = lspkind.cmp_format({
+      mode = 'symbol_text',
+    })
   },
   sources = {
+    { name = 'vsnip' },
     { name = 'nvim_lsp' },
     { name = 'buffer' },
     { name = 'path' },
