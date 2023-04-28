76bede8285218acdc72da46e140841ed5f823fdf
Ken Powers <ken@kenpowers.net>
2023-04-28 15:04:36 -0500

    nvim: customize lsp gutter icons

--- home/config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -88,3 +88,15 @@ mason_lspc.setup_handlers({
 
 -- Disable illuminate in fugitiveblame
 vim.g.Illuminate_ftblacklist = { 'fugitiveblame' }
+
+-- Custom gutter signs
+local signs = {
+  Error = ' ', Warn = ' ', Hint = ' ', Info = ' '
+}
+
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, {
+    text = icon, texthl = hl, numhl = hl
+  })
+end
