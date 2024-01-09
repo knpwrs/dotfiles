774caf6d5ae1370dad9d72aae6147731372520c0
Ken Powers <ken@kenpowers.net>
2024-01-09 16:11:43 -0500

    misc: new colorscheme

--- home/config/nvim/lua/config/lazy.lua
+++ home/config/nvim/lua/config/lazy.lua
@@ -82,7 +82,6 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = { enabled = false },
   performance = {
     rtp = {
--- home/config/nvim/lua/plugins/colorscheme.lua
+++ home/config/nvim/lua/plugins/colorscheme.lua
@@ -1,11 +1,8 @@
 return {
-  "folke/tokyonight.nvim",
-  opts = {
-    style = "night",
-    on_colors = function(colors)
-      colors.bg = "#000000"
-      colors.bg_dark = "#000000"
-      colors.bg_sidebar = "#000000"
-    end,
-  },
+  "EdenEast/nightfox.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd([[colorscheme carbonfox]])
+  end,
 }
