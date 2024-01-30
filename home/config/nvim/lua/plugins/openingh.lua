ff223c7ba98564a3a9536a68f8ae0d289f4a13ce
Ken Powers <ken@kenpowers.net>
2024-01-30 12:29:06 -0500

    nvim(openingh): add copy url maps

--- home/config/nvim/lua/plugins/openingh.lua
+++ home/config/nvim/lua/plugins/openingh.lua
@@ -3,10 +3,15 @@ return {
     "Almo7aya/openingh.nvim",
     keys = {
       { "<leader>gr", ":OpenInGHRepo<cr>", desc = "Open Repo" },
+      { "<leader>gcr", ":OpenInGHRepo+<cr>", desc = "Copy URL to Repo" },
       { "<leader>gf", ":OpenInGHFile!<cr>", desc = "Open File in Repo (Current Commit)" },
+      { "<leader>gcf", ":OpenInGHFile!+<cr>", desc = "Copy URL to File in Repo (Current Commit)" },
       { "<leader>gf", ":OpenInGHFile!<cr>", desc = "Open File in Repo (Current Commit)", mode = "v" },
+      { "<leader>gcf", ":OpenInGHFile!+<cr>", desc = "Copy URL to File in Repo (Current Commit)", mode = "v" },
       { "<leader>gF", ":OpenInGHFile<cr>", desc = "Open File in Repo" },
+      { "<leader>gcF", ":OpenInGHFile+<cr>", desc = "Copy URL to File in Repo" },
       { "<leader>gF", ":OpenInGHFile<cr>", desc = "Open File in Repo", mode = "v" },
+      { "<leader>gcF", ":OpenInGHFile+<cr>", desc = "Copy URL to File in Repo", mode = "v" },
     },
   },
 }
