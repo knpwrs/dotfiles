674a31a4623513d971d201044c42bc5937494c6a
Ken Powers <ken@kenpowers.net>
2024-01-30 11:25:39 -0500

    nvim: add openingh.nvim

---
+++ home/config/nvim/lua/plugins/openingh.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "Almo7aya/openingh.nvim",
+    keys = {
+      { "<leader>gr", ":OpenInGHRepo<cr>", desc = "Open Repo" },
+      { "<leader>gf", ":OpenInGHFile!<cr>", desc = "Open File in Repo (Current Commit)" },
+      { "<leader>gf", ":OpenInGHFile!<cr>", desc = "Open File in Repo (Current Commit)", mode = "v" },
+      { "<leader>gF", ":OpenInGHFile<cr>", desc = "Open File in Repo" },
+      { "<leader>gF", ":OpenInGHFile<cr>", desc = "Open File in Repo", mode = "v" },
+    },
+  },
+}
