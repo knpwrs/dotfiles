c11f8fa52db579174bf00456e439c68457d06cf8
Ken Powers <ken@kenpowers.net>
2024-03-19 16:40:39 -0400

    nvim: add neogit

--- home/config/nvim/lua/plugins/git.lua
+++ home/config/nvim/lua/plugins/git.lua
@@ -36,4 +36,16 @@ return {
       { "<leader>gb", "<cmd>ToggleBlame<cr>", desc = "Toggle Git Blame" },
     },
   },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim", -- required
+      "sindrets/diffview.nvim", -- optional - Diff integration
+      "nvim-telescope/telescope.nvim",
+    },
+    config = true,
+    keys = {
+      { "<leader>gn", "<cmd>Neogit<cr>", desc = "Open Neogit" },
+    },
+  },
 }
