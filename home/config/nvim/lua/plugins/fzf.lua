49639960c43dbab1c68c41c0fb591e608560eaeb
Ken Powers <ken@kenpowers.net>
2024-09-12 09:29:31 -0400

    nvim: fix fzf setup and update lazy-lock

--- home/config/nvim/lua/plugins/fzf.lua
+++ home/config/nvim/lua/plugins/fzf.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "nvim-telescope/telescope.nvim",
+    "ibhagwan/fzf-lua",
     keys = {
       { "<leader>fg", LazyVim.pick("live_grep"), desc = "Grep (Root Dir)" },
       { "<leader>fG", LazyVim.pick("live_grep", { root = false }), desc = "Grep (cwd)" },
