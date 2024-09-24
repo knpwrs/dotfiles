496210fb38563b95dd332ab1eb15dcd5b5ab8fa3
Ken Powers <ken@kenpowers.net>
2024-09-24 13:59:00 -0400

    wezterm: use WSL on windows

--- home/wezterm.lua
+++ home/wezterm.lua
@@ -7,4 +7,8 @@ config.font_size = 12.0
 
 config.hide_tab_bar_if_only_one_tab = true
 
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+  config.default_prog = { "wsl.exe" }
+end
+
 return config
