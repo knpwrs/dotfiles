275ea404f71c41cd644f2a5f9b44555c4e6cf77e
Ken Powers <ken@kenpowers.net>
2024-09-25 11:44:32 -0400

    wezterm: comment configuration file

--- home/wezterm.lua
+++ home/wezterm.lua
@@ -1,12 +1,15 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- Font
 config.color_scheme = "carbonfox"
 config.font = wezterm.font("Hack Nerd Font Mono")
 config.font_size = 12.0
 
+-- UI
 config.hide_tab_bar_if_only_one_tab = true
 
+-- Windows
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
   config.default_prog = { "wsl.exe" }
 end
