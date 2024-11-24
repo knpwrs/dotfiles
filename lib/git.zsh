53cf23e4f01334cd37f34dafe6f75fd77d134f2a
Ken Powers <ken@kenpowers.net>
2024-11-23 21:25:54 -0500

    lib(git): add kyrios to gcm alias

--- lib/git.zsh
+++ lib/git.zsh
@@ -9,7 +9,7 @@ function current_branch() {
 function git_main_branch() {
   command git rev-parse --git-dir &>/dev/null || return
   local branch
-  for branch in main trunk; do
+  for branch in main trunk kyrios; do
     if command git show-ref -q --verify refs/heads/$branch; then
       echo $branch
       return
