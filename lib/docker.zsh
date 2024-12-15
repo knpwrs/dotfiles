bbc0e339567e16065d6bd3f7e79db76b5f59684e
Ken Powers <ken@kenpowers.net>
2024-12-14 22:14:21 -0500

    navi(docker): docker-compose is now `docker compose`

--- lib/docker.zsh
+++ lib/docker.zsh
@@ -1,17 +1,17 @@
 # general
 alias deit='docker exec -it'
 
-# docker-compose
-alias dc="docker-compose"
-alias dcb="docker-compose build"
-alias dce="docker-compose exec"
+# docker compose
+alias dc="docker compose"
+alias dcb="docker compose build"
+alias dce="docker compose exec"
 dcl() {
   docker logs -f $(dc ps -q $1)
 }
-alias dck="docker-compose kill"
-alias dcrf="docker-compose rm -f"
-alias dcu="docker-compose up"
-alias dcud="docker-compose up -d"
-alias dcd="docker-compose down"
-alias dcr="docker-compose run --rm"
-alias dcrs="docker-compose restart"
+alias dck="docker compose kill"
+alias dcrf="docker compose rm -f"
+alias dcu="docker compose up"
+alias dcud="docker compose up -d"
+alias dcd="docker compose down"
+alias dcr="docker compose run --rm"
+alias dcrs="docker compose restart"
