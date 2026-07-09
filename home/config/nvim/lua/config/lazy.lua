ad72a713fa42d9dd752893f2371fa22de4fd5584
Ken Powers <ken@kenpowers.net>
2026-07-09 16:49:06 -0400

    nvim: enable go language extra

--- home/config/nvim/lua/config/lazy.lua
+++ home/config/nvim/lua/config/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.nix" },
